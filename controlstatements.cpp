fc5b8c475738222d1f54a2a793e7c1a1137a3073
tatsuki-pr <271235426+tatsuki-pr@users.noreply.github.com>
2026-03-29 22:21:56 +0530

    Used if elif ladder

--- controlstatements.cpp
+++ controlstatements.cpp
@@ -3,12 +3,40 @@ using namespace std;
 int main()
 {
     int i,b;
-    cout<<"How Far should the count be: ";
-    cin>>b;
-   for (i=0 ; i <=b ; i++)
-   {
-    cout<< i <<"\n";
-   }
+//     cout<<"How Far should the count be: ";
+//     cout<<"Shanu is gay>";
+//     cin>>b;
+//    for (i=0 ; i <=b ; i++)
+//    {
+//     cout<< i <<"\n";
+//    }
+cout<<"enter your age:";
+cin>>i;
+if (i<18 && i>0)
+{
+    cout<<"you cant come to my party\n";
+}
+else if (i==18)
+{
+    cout<<"you can come to my party but with a kids pass\n";
+    
+}
+else if (i>18)
+{
+    cout<<"you can come to my party\n";
+    
+}
+else 
+{
+    cout<<"invalid age! PLease TRY again\n";
+    
+}
+
+
+
+
+
+
     return 0;
 }
 
